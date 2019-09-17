1397ab7a008ed2a67b98b2d4a2f985fc4b465085
brandonTsai <brandonya125@gmail.com>
2019-09-17 01:27:35 -0700

    #2 Create mocha test (supertest)

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 const router = express.Router();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const ifPass = require("./helpers/ifPass");
 
