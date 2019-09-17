0d817f28a091549d5e790a708018b3dcea253a30
brandonTsai <brandonya125@gmail.com>
2019-09-17 00:37:56 -0700

    Add ifPass test cases

--- test/test.js
+++ test/test.js
@@ -1,12 +1,40 @@
 const supertest = require("supertest");
+const assert = require("assert");
 const should = require("should");
-const app = require('../server')
+const app = require('../server');
+const ifPass = require('../helpers/ifPass');
 
 // This agent refers to PORT where program is runninng.
 // const server = supertest.agent("http://localhost:8080");
 
 // UNIT test begin
 describe("POST login",function(){
+
+  it('should be true', function() {
+    const result = ifPass("Brandon666")
+    assert.equal(result, true)
+  });
+
+  it('should be true', function() {
+    const result = ifPass("brAndOn666")
+    assert.equal(result, true)
+  });
+
+  it('should be true', function() {
+    const result = ifPass("Br4ndon")
+    assert.equal(result, false)
+  });
+
+  it('should be true', function() {
+    const result = ifPass("brandon666")
+    assert.equal(result, false)
+  });
+
+  it('should be true', function() {
+    const result = ifPass("BrandonOHYA")
+    assert.equal(result, false)
+  });
+
   it('should pass supertest', function(done) {
     supertest(app)
     .post('/login')
